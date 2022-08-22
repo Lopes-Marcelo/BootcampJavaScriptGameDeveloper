ac22761ae6ecbcb2f37ae69c35666143ce01e73d
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-08-22 18:29:23 -0400

    Desafio E - Completo

--- Desafio de Código - Resolvendo Algoritmos com JavaScript/DesafioE.js
+++ Desafio de Código - Resolvendo Algoritmos com JavaScript/DesafioE.js
@@ -1,18 +1,35 @@
-const valorSalario = gets();
-const valorBeneficios = gets();
+let valorSalario = gets();
+let valorBeneficios = parseInt(gets());
 
-function calcularPorcentagem(valor, percentual) {
-  return valor * (percentual / 100);
+if(valorSalario >= 0 && valorSalario <= 1100){
+    salario = valorSalario * 0.95;
+} else if(valorSalario >= 1100.01 && valorSalario <= 2500.00){
+    salario = valorSalario * 0.9;
+} else {
+    salario = valorSalario * 0.85;
 }
 
-function pegarAliquota(salario) {
-  if (salario >= 0 && salario <= 1100) {
-    return 5;
-  } else if (salario >= 1100.01 && salario <= 2500.00) {
-    return 10;
-  } else {
-    return 15;
-  }
-}
+let total = (valorBeneficios + salario);
+print(total.toFixed(2));
 
-print(valor.toFixed(2));
+/*
+Desafio:
+Faça um programa que calcule e imprima o salário a ser transferido para um funcionário.
+
+Para realizar o calculo receba o valor bruto do salário e o adicional dos benefícios.
+O salário a ser transferido é calculado da seguinte maneira: 
+
+     Valor Bruto do salário - Percentual de Imposto Mediante a Faixa Salarial + Adicional dos Benefícios
+
+Para calcular o percentual de imposto segue as aliquotas:
+
+    De R$ 0.00 a R$ 1100.00 = 5.00%
+    De R$ 1100.01 a R$ 2500.00 = 10.00%
+    Maior que R$ 2500.00 = 15.00%
+
+Entrada
+A entrada consiste em vários arquivos de teste, que conterá o valor bruto do salário e adicional dos benefícios. Conforme mostrado no exemplo de entrada a seguir.
+
+Saída
+Para cada arquivo da entrada, terá um arquivo de saída. E como mencionado no Desafio, será gerado uma linha com um número que será a diferença entre o valor bruto do salário e o percentual de imposto mediante a faixa salárial somado com o adicional dos benefícios. 
+ */
